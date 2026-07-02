ade98caa76efe7b3cf679c2239e9399a02b2525e
hoangquang151004 <20220592@eaut.edu.vn>
2026-07-02 19:34:49 +0700

    them bai tap

---
+++ src/buoi_4/bai_1_ql_gio_hang.kt
@@ -0,0 +1,15 @@
+package buoi_4
+//Tạo một MutableList chứa tên 3 món ăn bạn thích.
+//Thêm 1 món mới vào cuối danh sách.
+//Xóa món ăn đầu tiên khỏi danh sách.
+//Dùng vòng lặp forEach (hoặc for) để in ra từng món ăn kèm theo dòng chữ "Tôi muốn ăn...".
+fun main(args: Array<String>) {
+    var monAn = mutableListOf("Chuoi","Cam","Quyt")
+    monAn.add("Sau rieng")
+    println("Cac mon an toi thich: "+ monAn)
+    monAn.remove("Chuoi")
+    println("Cac mon an toi thich con lai khi an mon dau: "+ monAn)
+    monAn.forEach{
+        println(it + " Toi muon an ... ")
+    }
+}
---
+++ src/buoi_4/foreach_index.kt
@@ -0,0 +1,8 @@
+package buoi_4
+
+var thuTu = listOf("Thu nhat", "thu hai", "thu ba")
+fun main() {
+    thuTu.forEachIndexed { index, xepHang ->
+        println("Vi tri $index co xep hang la: "+ xepHang)
+    }
+}
